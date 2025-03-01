787c77beccfa117fee9ab49c6fa85644fb9b34b5
kTrzcinskii <kt.trzcinski@gmail.com>
2025-03-01 13:47:28 +0100

    Fix refraction

--- src/extensions/vector_refraction.rs
+++ src/extensions/vector_refraction.rs
@@ -6,13 +6,11 @@ pub trait VectorRefractionExtension<T> {
 
 impl<T: RealField> VectorRefractionExtension<T> for UnitVector3<T> {
     fn refract(&self, normal: &UnitVector3<T>, etai_over_etat: f64) -> Vector3<T> {
-        let cos_theta = -self.dot(normal);
+        let cos_theta = (-self.dot(normal)).min(T::one());
         let out_perpendicular =
             (self.as_ref() + normal.as_ref() * cos_theta) * T::from_f64(etai_over_etat).unwrap();
-        let out_parallel = normal.as_ref()
-            * (T::from_f64(1.0).unwrap() - out_perpendicular.norm_squared())
-                .abs()
-                .sqrt();
+        let out_parallel =
+            -normal.as_ref() * (T::one() - out_perpendicular.norm_squared()).abs().sqrt();
         out_parallel + out_perpendicular
     }
 }
